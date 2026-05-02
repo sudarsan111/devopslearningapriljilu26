18d729ba409a0e1a051812290c9903dedb920ea5
Devops Learning <56231803+sudarsan111@users.noreply.github.com>
2026-05-02 10:52:30 +0530

    all change for git hub manuall workflow

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,6 @@
-string = "jilu"
-print(string)
+import sys
+
+# Get the name from the command line argument if provided, otherwise default to "jilu"
+name = sys.argv[1] if len(sys.argv) > 1 else "jilu"
+
+print(f"Hello {name}!")
